94951c396842639cf7a220e73ab1cebc88a26f3f
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-09 17:13:46 +0530

    Add files via upload

---
+++ 800/Prepend_and_Append.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Prepend_and_Append {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            String s=sc.next();
+            int left=0,right=n-1;
+
+            while(left<right && s.charAt(left)!=s.charAt(right)){
+                left++;
+                right--;
+            }
+            System.out.println(right-left+1);
+        }
+        sc.close();
+    }
+}
