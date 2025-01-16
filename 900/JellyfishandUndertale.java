558a23b1115134ed6f7a6d5aec91e51c213422ac
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-16 21:06:55 +0530

    Add files via upload

---
+++ 900/JellyfishandUndertale.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class JellyfishandUndertale {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt(); 
+    for (int tcase = 1; tcase <= t; tcase++) {
+        long a = sc.nextLong();
+        long b = sc.nextLong(); 
+        long n = sc.nextLong(); 
+
+        long ans = b - 1; 
+        for (int i = 0; i < n; i++) {
+            long ai = sc.nextLong();
+            if (ai <= a - 1) {
+                ans += ai;
+            } else {
+                ans += a - 1;
+            }
+        }
+        ans++; 
+        System.out.println(ans);
+    }
+    sc.close();
+}
+}
