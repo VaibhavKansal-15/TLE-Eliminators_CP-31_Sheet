f2615f3c4fb203370d6403d69559596f5c6c8b4d
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-29 20:23:32 +0530

    Add files via upload

---
+++ 900/DeletiveEditing.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class DeletiveEditing {
+    private static void answer(boolean v) {
+        System.out.println(v ? "YES" : "NO");
+    }
+
+    private static void solve(String s, String t) {
+        int[] f = new int[26];
+        for (char c : t.toCharArray()) {
+            f[c - 'A']++;
+        }
+
+        StringBuilder r = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (f[c - 'A']-- > 0) {
+                r.append(c);
+            }
+        }
+
+        answer(r.reverse().toString().equals(t));
+    }
+
+    private static void testCase(Scanner sc) {
+        String s = sc.next();
+        String t = sc.next();
+        solve(s, t);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            testCase(sc);
+        }
+        sc.close();
+    }
+}
