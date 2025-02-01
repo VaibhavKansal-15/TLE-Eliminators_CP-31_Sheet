08549a839ad17724e7fe430205d3372adffc6f84
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-01 21:55:03 +0530

    Update ForbiddenInteger.java

--- 800/ForbiddenInteger.java
+++ 800/ForbiddenInteger.java
@@ -3,12 +3,10 @@ public class ForbiddenInteger {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt(); 
-        
         while (t-- > 0) {
             int n = sc.nextInt();
             int k = sc.nextInt(); 
             int x = sc.nextInt(); 
-            
             if (x == 1 && k == 1) {
                 System.out.println("NO");
             } else if (x == 1 && k == 2 && n % 2 == 1) {
