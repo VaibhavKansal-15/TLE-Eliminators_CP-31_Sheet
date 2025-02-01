273c1642dfbefc2e5424f67d544daf990f96b43d
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-01 22:02:46 +0530

    Add files via upload

---
+++ 900/OddDivisor.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OddDivisor {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0){
+      long n=sc.nextLong();
+      if(n%2!=0 || !isPower(n)){
+        System.out.println("YES");
+      }else{
+        System.out.println("NO");
+      }
+    }
+    sc.close();
+  }
+
+  private static boolean isPower(long n){
+    return (n & (n-1))==0;
+  }
+}
