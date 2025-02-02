06a4df9daaf08bc884f3d9569e9b142e3e5f203d
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-02 22:08:09 +0530

    Add files via upload

---
+++ 900/BadBoy.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BadBoy {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0){
+      long n=sc.nextLong(),m=sc.nextLong();
+      long i=sc.nextLong(),j=sc.nextLong();
+
+      System.out.println("1"+" "+"1"+" "+n+" "+m);
+    }
+    sc.close();
+  }
+}
