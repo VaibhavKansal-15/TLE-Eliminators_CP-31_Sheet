e24d17c223c38b56829d19401d77b9f6741a9aad
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-02 21:44:36 +0530

    Add files via upload

---
+++ 900/ExcitingBets.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ExcitingBets {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0){
+      long a=sc.nextLong();
+      long b=sc.nextLong();
+      if(a==b){
+        System.out.println("0 0");
+      }else{
+        long gcd=Math.abs(a-b);
+        long moves = Math.min(a % gcd, gcd - (a % gcd));
+        System.out.println(gcd+" "+moves);
+      }
+    }
+    sc.close();
+  }
+}
