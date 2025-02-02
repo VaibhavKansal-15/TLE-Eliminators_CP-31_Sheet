3d6854abf9a4dfadc17880756665d5d41443693f
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-02 21:26:12 +0530

    Add files via upload

---
+++ 900/MochaAndMath.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class MochaAndMath {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0){
+      int n=sc.nextInt();
+      int count=sc.nextInt();
+      for(int i=1;i<n;i++){
+        count&=sc.nextInt();
+      }
+      System.out.println(count);
+    }
+    sc.close();
+  }
+}
