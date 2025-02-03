3ef0ed7b45802e09e5f45dbc5492d5641d550bfb
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-03 22:52:31 +0530

    Update DeletiveEditing.java

--- 900/DeletiveEditing.java
+++ 900/DeletiveEditing.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
-
 public class DeletiveEditing {
     private static void answer(boolean v) {
         System.out.println(v ? "YES" : "NO");
     }
-
     private static void solve(String s, String t) {
         int[] f = new int[26];
         for (char c : t.toCharArray()) {
             f[c - 'A']++;
         }
-
         StringBuilder r = new StringBuilder();
         for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
@@ -18,16 +15,13 @@ public class DeletiveEditing {
                 r.append(c);
             }
         }
-
         answer(r.reverse().toString().equals(t));
     }
-
     private static void testCase(Scanner sc) {
         String s = sc.next();
         String t = sc.next();
         solve(s, t);
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
