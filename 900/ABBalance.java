8b4ef5fc84eeee5c6ad860a5e8554ba1e2633f70
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-03 22:50:33 +0530

    Update ABBalance.java

--- 900/ABBalance.java
+++ 900/ABBalance.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class ABBalance {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
@@ -8,7 +7,6 @@ public class ABBalance {
     while(t-->0){
       String s=sc.nextLine();
       int n=s.length();
-
       int abCount=0;
       int baCount=0;
       for(int i=0;i<n-1;i++){
@@ -18,19 +16,16 @@ public class ABBalance {
           baCount++;
         }
       }
-
       if(abCount==baCount){
         System.out.println(s);
         continue;
       }
-
       char[] chars=s.toCharArray();
       if(abCount>baCount){
         chars[n-1]='a';
       }else{
         chars[n-1]='b';
       }
-
       System.out.println(new String(chars));
     }
     sc.close();
