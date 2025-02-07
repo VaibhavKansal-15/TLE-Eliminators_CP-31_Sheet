0ed73050cea220273ba28bcd8d8febc8cbce128d
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-07 23:54:11 +0530

    Update ThreeIndices.java

--- 900/ThreeIndices.java
+++ 900/ThreeIndices.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class ThreeIndices {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
@@ -7,11 +6,9 @@ public class ThreeIndices {
     while(t-->0){
       int n=sc.nextInt();
       int[] arr=new int[n];
-
       for(int i=0;i<n;i++){
         arr[i]=sc.nextInt();
       }
-
       boolean found = false;
       for (int j = 1; j < n - 1; j++) {
         if (arr[j - 1] < arr[j] && arr[j] > arr[j + 1]) {
