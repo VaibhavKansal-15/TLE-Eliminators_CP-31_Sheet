38f5110ac72968f314c59e76050e332022133321
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-16 14:19:48 +0530

    Update SwapAndDelete.java

--- 1000/SwapAndDelete.java
+++ 1000/SwapAndDelete.java
@@ -1,1 +1,30 @@
-put your content back in it
+import java.util.Scanner;
+
+public class SwapAndDelete {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String str = sc.next();
+            int zero = 0, one = 0;
+            
+            for (char c : str.toCharArray()) {
+                if (c == '0') zero++;
+                else one++;
+            }
+
+            for (char c : str.toCharArray()) {
+                if (c == '0') {
+                    if (one > 0) one--;
+                    else break;
+                } else {
+                    if (zero > 0) zero--;
+                    else break;
+                }
+            }
+
+            System.out.println(zero + one);
+        }
+        sc.close();
+    }
+}
