ea391121581e98ef32082895be1e6cbcd3a17294
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-18 23:03:54 +0530

    Update Multiplyby2divideby6.java

--- 900/Multiplyby2divideby6.java
+++ 900/Multiplyby2divideby6.java
@@ -7,17 +7,14 @@ public class Multiplyby2divideby6 {
         while (t-- > 0) {
             long n = sc.nextLong();
             int count2 = 0, count3 = 0;
-
             while (n % 2 == 0) {
                 n /= 2;
                 count2++;
             }
-
             while (n % 3 == 0) {
                 n /= 3;
                 count3++;
             }
-
             if (n != 1 || count2 > count3) {
                 System.out.println(-1);
             } else {
