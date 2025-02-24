1a854c8e7f36f8a88a1f5c7fd5a6e64180d6e42a
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-24 21:46:25 +0530

    Update MainakandArray.java

--- 900/MainakandArray.java
+++ 900/MainakandArray.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class MainakandArray{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
@@ -9,21 +10,17 @@ public class MainakandArray{
       for(int i=0;i<n;i++){
         arr[i]=sc.nextInt();
       }
-
       if (n == 1) {
         System.out.println(0);
         continue;
       }
-      
       int maxDiff=Integer.MIN_VALUE;
       for(int i=1;i<n;i++){
         maxDiff=Math.max(maxDiff,arr[i]-arr[0]);
       }
-
       for(int i=0;i<n-1;i++){
         maxDiff=Math.max(maxDiff,arr[n-1]-arr[i]);
       }
-
       for(int i=0;i<n-1;i++){
         maxDiff=Math.max(maxDiff,arr[i]-arr[i+1]);
       }
