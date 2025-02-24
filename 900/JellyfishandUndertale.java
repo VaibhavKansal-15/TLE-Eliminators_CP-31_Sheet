89ed7a681feecac9ba9f9d5147bb70bc24687d43
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-02-24 21:45:20 +0530

    Update JellyfishandUndertale.java

--- 900/JellyfishandUndertale.java
+++ 900/JellyfishandUndertale.java
@@ -3,13 +3,11 @@ import java.util.*;
 public class JellyfishandUndertale {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
     int t = sc.nextInt(); 
     for (int tcase = 1; tcase <= t; tcase++) {
         long a = sc.nextLong();
         long b = sc.nextLong(); 
         long n = sc.nextLong(); 
-
         long ans = b - 1; 
         for (int i = 0; i < n; i++) {
             long ai = sc.nextLong();
@@ -23,5 +21,5 @@ public class JellyfishandUndertale {
         System.out.println(ans);
     }
     sc.close();
-}
+  }
 }
