371774ea0b2da68e4589914bf776f76d6083ff37
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-03-03 23:04:28 +0530

    Update HalloumiBoxes.java

--- 800/HalloumiBoxes.java
+++ 800/HalloumiBoxes.java
@@ -4,22 +4,18 @@ public class HalloumiBoxes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long t = sc.nextLong(); // Number of test cases
-
         while (t-- > 0) {
             int n = sc.nextInt(); // Number of boxes
             int k = sc.nextInt(); // Maximum subarray length for reversal
-
             long[] arr = new long[n];
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextLong();
             }
-
             // Case 1: If k >= 2, always possible to sort
             if (k >= 2) {
                 System.out.println("YES");
                 continue;
             }
-
             // Case 2: If k == 1, check if the array is already sorted
             boolean isSorted = true;
             for (int i = 1; i < n; i++) {
@@ -34,7 +30,6 @@ public class HalloumiBoxes {
                 System.out.println("NO");
             }
         }
-
         sc.close();
     }
 }
