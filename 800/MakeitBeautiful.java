b6f01dcbbead749c5c54e3e97f944cc6dbffa3d7
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-03-03 23:06:45 +0530

    Update MakeitBeautiful.java

--- 800/MakeitBeautiful.java
+++ 800/MakeitBeautiful.java
@@ -5,7 +5,6 @@ public class MakeitBeautiful {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
-
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
@@ -14,7 +13,6 @@ public class MakeitBeautiful {
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(input[i]);
             }
-
             Arrays.sort(a);
             int count = 1;
             for (int i = 1; i < n; i++) {
